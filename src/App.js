5c447630668604f9af02846a527894783885022d
tien <tien199013@hotmail.com>
2023-11-11 10:22:34 +0000

    bind keydown and add display name

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Drum from './Drum';
 
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -10,19 +9,25 @@ class App extends React.Component {
     this.state = {
       currentKick: ''
     }
+
+    this.updateDisplay = this.updateDisplay.bind(this);
   }
 
   kicks = [
-    { id: "Q", audio_src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" },
-    { id: "W", audio_src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" },
-    { id: "E", audio_src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" },
-    { id: "A", audio_src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" },
-    { id: "S", audio_src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" },
-    { id: "D", audio_src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" },
-    { id: "Z", audio_src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" },
-    { id: "X", audio_src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" },
-    { id: "C", audio_src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" },
+    { id: "Q", name: "Heater-1", audio_src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" },
+    { id: "W", name: "Heater-2", audio_src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" },
+    { id: "E", name: "Heater-3", audio_src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" },
+    { id: "A", name: "Heater-4", audio_src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" },
+    { id: "S", name: "Clap", audio_src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" },
+    { id: "D", name: "Open-HH", audio_src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" },
+    { id: "Z", name: "Kick-n'-Hat", audio_src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" },
+    { id: "X", name: "Kick", audio_src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" },
+    { id: "C", name: "Close-HH", audio_src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" },
   ]
+
+  updateDisplay() {
+
+  }
 
   render() {
     return (
@@ -31,7 +36,7 @@ class App extends React.Component {
 
           {
             this.kicks.map(kick =>
-              <Drum id={kick.id} src={kick.audio_src}></Drum>
+              <Drum id={kick.id} src={kick.audio_src} displayName={kick.name} updateDisplay={this.updateDisplay}></Drum>
             )
           }
         </div>
--- src/Drum.js
+++ src/Drum.js
@@ -3,11 +3,36 @@ import React from "react";
 class Drum extends React.Component {
     constructor(props) {
         super(props)
+        this.click = this.click.bind(this)
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+    }
+
+    componentDidMount() {
+        document.addEventListener("keydown", this.handleKeyPress);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener("keydown", this.handleKeyPress);
+    }
+
+    handleKeyPress(event) {
+        if (event.key.toUpperCase() === this.props.id) {
+            this.click();
+        }
+    }
+
+    click() {
+        const drum = document.getElementById(this.props.id);
+        drum.currentTime = 0;
+        drum.play();
+        // this.activatePad(),
+        // setTimeout((() => this.activatePad()), 100),
+        this.props.updateDisplay(this.props.displayName);
     }
 
     render() {
         return (
-            <div className="drum-pad">
+            <div className="drum-pad" onClick={this.click}>
                 <audio className="clip" id={this.props.id} src={this.props.src}></audio>
                 {this.props.id}
             </div>
